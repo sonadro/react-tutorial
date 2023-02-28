25fe67337886c7ef3d741e0304b484df15a4d5e4
sonadro <sondre.rooth@gmail.com>
2023-02-28 15:17:56 +0100

    ep 8

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('mario');
+
+    const handleClick = () => {
+        setName('luigi');
+    };
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
